3d5b27d583a22e5437286ca09803bc0f9d713b14
a-drewsky <dembofskya@gmail.com>
2020-10-17 16:21:54 -0400

    fix

--- app.js
+++ app.js
@@ -7,6 +7,6 @@ app.get('/', function(req, res){
 });
 app.use('/client', express.static(__dirname + '/client'));
 
-serv.listen(2000);
+serv.listen(process.env.PORT || 2000);
 
 console.log("Server Started");
